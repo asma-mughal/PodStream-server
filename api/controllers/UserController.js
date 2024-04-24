17d8aaf3c33ccc48e61cbf2553b59bdaff15e3d2
Muhammad Yaseen <Yaseen@Muhammads-MacBook-Pro.local>
2024-04-24 08:31:08 +0500

    Work

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -1,27 +1,25 @@
-
 import { User } from "../models/User.js";
-import JWT from 'jsonwebtoken';
+import JWT from "jsonwebtoken";
 const customHash = (str) => {
-    let hash = 0;
+  let hash = 0;
 
-    if (str.length === 0) {
-        return hash;
-    }
-
-    for (let i = 0; i < str.length; i++) {
-        const char = str.charCodeAt(i);
-        hash = ((hash << 5) - hash) + char;
-        hash |= 0; // Convert to 32-bit integer
-    }
-
+  if (str.length === 0) {
     return hash;
-}
+  }
+
+  for (let i = 0; i < str.length; i++) {
+    const char = str.charCodeAt(i);
+    hash = (hash << 5) - hash + char;
+    hash |= 0; // Convert to 32-bit integer
+  }
+
+  return hash;
+};
 
 export const SignUp = async (req, res) => {
-
   try {
-    const { name, email, password} = req.body;
-    if (!name || !email || !password ) {
+    const { name, email, password } = req.body;
+    if (!name || !email || !password) {
       return res.status(400).json({ message: "Bad Request", success: false });
     }
     const userEmail = await User.find({ email }).lean();
@@ -29,22 +27,20 @@ export const SignUp = async (req, res) => {
       return res
         .status(400)
         .json({ message: "Email already exists", success: false });
-    } 
+    }
     const hashPassword = customHash(password);
 
     const newUser = new User({
-      userName : name,
+      userName: name,
       email,
       password: hashPassword,
     });
     const savedUser = await newUser.save();
-    return res
-      .status(200)
-      .json({
-        message: "User created successfully",
-        success: true,
-        user: savedUser,
-      });
+    return res.status(200).json({
+      message: "User created successfully",
+      success: true,
+      user: savedUser,
+    });
   } catch (error) {
     console.error("Error creating user:", error);
     return res
@@ -54,37 +50,69 @@ export const SignUp = async (req, res) => {
 };
 
 export const SignIn = async (req, res) => {
-    try {
-      const {  email, password} = req.body;
-      if ( !email || !password ) {
-        return res.status(400).json({ message: "Bad Request", success: false });
-      }
-        const foundUser = await User.findOne({ email }).lean();
-      if (foundUser.length < 0) {
-        return res
-          .status(400)
-          .json({ message: "Email Doesn't exists", success: false });
-      }
-        const hashedUserInputPassword = customHash(password);
-        if (foundUser?.password != hashedUserInputPassword) {
-            return res.status(200).json({ message: "Password doesn't match" });
-        }
-        const token = JWT.sign({ _id: foundUser?.id }, "test", {
-            expiresIn:"4d"
-        })
-
-        return res.status(201).json({
-            message: "User LoggedIn successfully", user: {
-              _id: foundUser?._id,
-              name: foundUser?.userName,
-              email: foundUser?.email,
-              phone: foundUser?.phone,
-              adddress: foundUser?.address,
-      } , token });
-    } catch (error) {
-      console.error("Error creating user:", error);
-      return res
-        .status(500)
-        .json({ message: "Internal Server Error", success: false });
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.status(400).json({ message: "Bad Request", success: false });
     }
-  };
+    const foundUser = await User.findOne({ email }).lean();
+    if (foundUser.length < 0) {
+      return res
+        .status(400)
+        .json({ message: "Email Doesn't exists", success: false });
+    }
+    const hashedUserInputPassword = customHash(password);
+    if (foundUser?.password != hashedUserInputPassword) {
+      return res.status(200).json({ message: "Password doesn't match" });
+    }
+    const token = JWT.sign({ _id: foundUser?.id }, "test", {
+      expiresIn: "4d",
+    });
+
+    return res.status(201).json({
+      message: "User LoggedIn successfully",
+      user: {
+        _id: foundUser?._id,
+        name: foundUser?.userName,
+        email: foundUser?.email,
+      },
+      token,
+    });
+  } catch (error) {
+    console.error("Error creating user:", error);
+    return res
+      .status(500)
+      .json({ message: "Internal Server Error", success: false });
+  }
+};
+export const ResetPassword = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.status(400).json({ message: "Bad Request", success: false });
+    }
+    const foundUser = await User.findOne({ email }).lean();
+    if (foundUser.length < 0) {
+      return res
+        .status(400)
+        .json({ message: "Email Doesn't exists", success: false });
+    }
+    const hashPassword = customHash(password);
+    const updateUser = await User.findByIdAndUpdate(
+      foundUser?._id,
+      { password: hashPassword },
+      { lean: true }
+    );
+    return res.status(201).json({
+      message: "Password updated successfully",
+      user: {
+        name: updateUser?.userName,
+      },
+    });
+  } catch (error) {
+    console.error("Error updating Password:", error);
+  return res
+    .status(500)
+    .json({ message: "Internal Server Error", success: false });
+}
+};
--- api/routes/userRoute.js
+++ api/routes/userRoute.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { SignIn, SignUp } from '../controllers/UserController.js';
+import { ResetPassword, SignIn, SignUp } from '../controllers/UserController.js';
 const router = express.Router()
 
 
 router.post('/login', SignIn)
 router.post('/register', SignUp)
+router.put('/forgetPass', ResetPassword)
 
 
 
